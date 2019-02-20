73801f82d1ed0754a5ba09338c61af24aa1bb0ab
alecnicolas <alecnmarcum@gmail.com>
2019-02-20 13:03:38 -0800

    set up sass and fonts

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -15,6 +15,6 @@
 
 <script>
     export default {
-      
+
     }
 </script>
--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue SPA Demo</title>
 </head>
