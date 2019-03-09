efdda8790f316aa328300e3a1177023c9a87bb28
alecnicolas <alecnmarcum@gmail.com>
2019-03-08 16:32:13 -0800

    actually used the gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -42175,8 +42175,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/andrewgrano/websites/badges/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/andrewgrano/websites/badges/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\amzer\Desktop\Work\takeoff\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\amzer\Desktop\Work\takeoff\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
