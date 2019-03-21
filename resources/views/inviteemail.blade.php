0504f736559ce3fc2376e8b5f7d9c8855bad5628
Raima Hossain <dragdo3@gmail.com>
2019-03-21 13:28:01 -0700

    committing

--- resources/views/inviteemail.blade.php
+++ resources/views/inviteemail.blade.php
@@ -1,12 +1,12 @@
-@extends('spa')
-<div>
+
+<div class="container-fluid">
     <div class="email-top justify-content-center">
-        <h1 class="text-center">Svg goes below</h1>
+        <span class="text-center">Svg goes below</span>
     </div>
     <div class="email-bottom justify-content-center">
         <div class="pt-5">
-            <span class="text-center email__text d-block"> Welcome to Takeoff!! </span>
-            <span class="text-center email__text d-block"> Here is your access code </span>
+            <div class="text-center email__text mx-auto"> Welcome to Takeoff!! </div>
+            <div class="text-center email__text mx-auto"> Here is your access code </div>
         </div>
         <div>
             <button type="button" id="access" class="btn btn-light email__access mx-auto d-block"></button>
@@ -19,8 +19,46 @@
     </div>
 </div>
 
+<style>
+.email-top {
+    background: #D5D7EB;
+    height: 40%;
+    width: 100%
+}
+
+.email-bottom {
+    background: #7F83B1;
+    height: 60%;
+    width: 100%;
+}
+
+.email__text {
+    color: #FCFBFB;
+    font-size: 26px;
+    font-weight: 200;
+    line-height: 2.5rem;
+}
+
+.email__access {
+    color: black;
+    border: 1px #DD7D65 dotted;
+    border-radius: 50px;
+    padding-left: 50px;
+    padding-right: 50px;
+    font-size: 1.2rem;
+    margin: 2rem;
+}
+
+.email__signup {
+    text-decoration: underline;
+    color: #FCFBFB;
+    font-size: 1.2rem;
+}
+</style>
+
 <script>
     document.getElementById('access').innerHTML= 'A2JK76X90';
 </script>
-@section('content')
+
+@section ('content')
 @endsection
