1457971c51fdb292c61f85b80fffd907cf6833d8
Mikkal <mcnultymikkal@gmail.com>
2019-04-30 09:58:21 -0700

    TKO-79: establish model and migration for the workshops

---
+++ database/migrations/2019_04_29_181306_create_workshops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('instructor');
+            $table->string('about_instructor');
+            $table->string('assignment_info');
+            $table->string('workshop_name');
+            $table->string('workshop_description');
+            $table->time('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
