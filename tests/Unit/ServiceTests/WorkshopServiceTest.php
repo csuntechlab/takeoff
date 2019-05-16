9344eda95a1c4147d7f53e138fff8cb60cbeedad
Mikkal <mcnultymikkal@gmail.com>
2019-05-16 09:27:20 -0700

    TKO-81: add test

--- tests/Unit/ServiceTests/WorkshopServiceTest.php
+++ tests/Unit/ServiceTests/WorkshopServiceTest.php
@@ -3,6 +3,8 @@
 namespace tests\Unit\ServiceTests;
 
 use App\Models\Workshop;
+use App\Models\User;
+use App\Models\UserInfo;
 use App\Services\WorkshopService;
 use App\ModelRepositoryInterfaces\WorkshopModelRepositoryInterface;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -64,5 +66,47 @@ class WorkshopServiceTest extends TestCase
         $mockWorkshop->save();
 
         $this->assertEquals($mockWorkshop->toArray(), $this->service->getWorkshop($mockId));
+    }
+
+     /**
+     * @test
+     * @group noFramework
+     */
+    public function getAttendance_returns_an_array() {
+        $mockId = '1';
+        $mockUser = new User(['id' => '1', 'first_name' => 'mikkal', 'last_name' => 'mcnulty', 'user_id' => '1', 'email' => 'test@test.com']);
+        $mockUserInfo = new UserInfo(['id' => '1',
+         'first_name' => 'mikkal',
+         'last_name' => 'mcnulty',
+         'grad_date' => 'june',
+         'college' => 'Computer Science',
+         'bio' => 'hi there',
+         'research' => 'assa',
+         'fun_facts' => 'asfsdsadf',
+         'academic_interest' => 'asfds',
+         'title' => 'asdasd', 'user_id' => '1',
+         'major' => 'test',
+         'archive' => '0',
+         'email' => 'test@test.com']);
+
+        $mockWorkshop = new Workshop([
+            'id' => $mockId,
+            'workshop_name' => 'asdasd',
+            'workshop_description' => 'asdasdasd',
+            'instructor' => 'Mikkal',
+            'about_instructor' => 'he cool',
+            'assignment_info' => 'asdasd',
+            'date' => '03/03/9999'
+        ]);
+
+        $mockUserInfo->save();
+        $mockUser->save();
+        $mockWorkshop->save();
+        $mockWorkshop->users()->attach($mockUser->id);
+        $mockWorkshop->save();
+
+        // dd($mockUserInfo);
+        // dd($this->service->getAttendance(1)[0]);
+        $this->assertIsArray($this->service->getAttendance(1));
     }
 }
